35910936811e6e01ffe727a00a6a8fd373a83928
Tirth S Pandya <teamfznltd@gmail.com>
2024-10-08 20:22:44 +0530

    commit

---
+++ arrays.ts
@@ -0,0 +1,6 @@
+// type of object that can store multiple values of same data type in ts you have to specify the datatype
+// using square bracket notation
+// using array<type>
+
+const nums: number[] = [1,2,3];
+console.log(nums);
