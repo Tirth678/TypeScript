496c9ffb9455039dc28c6c9ef143d5daf5ae7c9d
Tirth S Pandya <teamfznltd@gmail.com>
2024-10-08 20:31:16 +0530

    commit

--- arrays.ts
+++ arrays.ts
@@ -4,3 +4,22 @@
 
 const nums: number[] = [1,2,3];
 console.log(nums);
+
+const arrays: string[] = ["this", "is", "nothing"];
+console.log(arrays);
+
+const newN: string[] = [];
+newN.push("mouse");
+console.log(newN);
+
+
+// new way to define any type of array
+const items : Array<string> = [];
+console.log(items);
+
+
+const items2: Array<number> = [1,2,3,"nothing"];
+// string will not be taking as a element
+console.log(items2);
+
+// multi-dimensional arrays
