db4109b393818451ececeb5f4f3452113b2b271d
Tirth S Pandya <teamfznltd@gmail.com>
2024-10-08 19:34:47 +0530

    commit

---
+++ functions.ts
@@ -0,0 +1,10 @@
+// this is a regular function
+function addOne(num: number){
+    return num+1;
+}
+const result = addOne(2);
+console.log(result);
+
+
+// arrow function
+function double(x: number,y: number) => x*y;
