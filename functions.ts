ad76e093d69fa0f29f86a17dccc08fde741dcc56
Tirth S Pandya <teamfznltd@gmail.com>
2024-10-08 19:56:59 +0530

    commit

--- functions.ts
+++ functions.ts
@@ -7,4 +7,47 @@ console.log(result);
 
 
 // arrow function
-function double(x: number,y: number) => x*y;
+const double = (x: number,y: number) => x * y;
+const result2 = double(1,2);
+console.log(result2);
+
+
+// default parameter values
+function greet(person: string){
+    return `Hello ${person}`;
+}
+console.log(greet("huxn"));
+
+
+// functions return types
+
+// normal function
+// arrow function
+
+// void return
+// void is the type which does not return anything or any value
+function printVoid(message: string): void{
+    console.log(`this function dosent give a ${message}`);
+}
+printVoid("nothing");
+
+
+// never
+// this is when function will not return anything
+// never:
+// a function has an infinte loop
+// a function that throws an error
+// variable that can never have a value
+
+function error(msg: string): never {
+    while (true) {};
+}
+
+// example
+let x: never;
+function returnValue(): never {
+    while(true){};
+}
+x = returnValue();
+// this line will cause a compile time error cause function never returns
+
