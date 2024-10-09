8cd410263d9722c8c53ba99a820b2346f7e911a1
Tirth S Pandya <teamfznltd@gmail.com>
2024-10-09 19:33:43 +0530

    Update typeAlias.ts
    commit

--- typeAlias.ts
+++ typeAlias.ts
@@ -14,3 +14,25 @@ type User = {
 
   const result = printUserInfo({ name: "Alex", age: 20, location: "USA" });
   console.log(result);
+
+  type person = {
+    Name: string,
+    Gender: string,
+    Enroll: number,
+    email?: string,
+  };
+
+  // now we are gonna make an object of a person we can choose weather to include email or not
+  const alice: person = {
+    Name: "Alice",
+    Gender: "Male",
+    Enroll: 12,
+  };
+  const Harman: person = {
+    Name: "Harman",
+    Gender: "Female",
+    Enroll: 320,
+    email: "example@service.com"
+  };
+  // here email is with ? which means that object property is optional to include as in ts whatever is defined has to be used stricly
+  
