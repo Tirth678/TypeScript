a3b63f4a209e1bf2b5fa059d12145cbc7f6c89c5
Tirth S Pandya <teamfznltd@gmail.com>
2024-10-09 18:58:47 +0530

    commit

--- typeAlias.ts
+++ typeAlias.ts
@@ -2,3 +2,15 @@
 type Mystring = string;
 
 
+type User = {
+    name: string;
+    age: number;
+    location: string;
+  };
+
+  const printUserInfo = (user: User) => {
+    return `Name: (${user.name}) Age: (${user.age}) Location: (${user.location})`;
+  };
+
+  const result = printUserInfo({ name: "Alex", age: 20, location: "USA" });
+  console.log(result);
