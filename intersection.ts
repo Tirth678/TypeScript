8d5f0ebdc43c3deb03500d5b8aeb252d68caa0d1
Tirth S Pandya <teamfznltd@gmail.com>
2024-10-09 21:50:09 +0530

    commit

--- intersection.ts
+++ intersection.ts
@@ -1,2 +1,29 @@
 // a way to combine to combine multiple types into one and define that includes all the properties and methods of each constitute type
 
+// 1.
+type Person = {
+    name: string,
+    age: number,
+    employeeId: number,
+};
+
+type Employee = {
+    id: number,
+    ClassNo: number,
+    divsion: string,
+};
+
+// 2.
+
+type PersonAndEmployee = Person & Employee;
+
+
+// 3.
+const merge: PersonAndEmployee = {
+    name: "Aniket",
+    age: 12,
+    employeeId: 46,
+    id: 32,
+    ClassNo: 12,
+    divsion: "A",
+};
---
+++ union.ts
@@ -0,0 +1,28 @@
+// unions are used to define a type that can have one of several possible types are useful when we want to allow a variable or parameter to accept multiple values
+// | to define a union
+
+let password: string | number = 20;
+
+type UserInfo = {
+  first: string;
+  last: string;
+  age: number;
+};
+
+type AccountDetails = {
+  email: string;
+  password: string;
+};
+
+let user: UserInfo | AccountDetails = {
+  first: "HuXn",
+  last: "WebDev",
+  age: 29,
+  email: "something@fmail.com",
+  password: `nothingbut123`
+};
+
+console.log(user);
+
+const item: (number | string)[] = [1, 3, "hello"];
+console.log(item);
